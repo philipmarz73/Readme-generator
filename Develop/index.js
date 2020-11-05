474932f37de0c2487003c2dea3826df8debd9c1f
Phlipper <marzullop73@gmail.com>
2020-11-05 17:18:38 -0500

    added required files and console logged a test question in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,17 @@
 const inquirer = require('inquirer');
-
-
+const generateMarkdown = require ("./utils/generateMarkdown.js")
+const fs = require('fs');
+// const path = require('path');
 // array of questions for user
 const questions = [
+    
+            {
+                type: 'input',
+                message: 'what is your name',
+                name: 'title',
+                
+            }
+        
 
 
 ];
@@ -10,11 +19,16 @@ console.log("hello World!");
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then((response) => {
+        console.log(response)
+        writeToFile("ReadMe.md", generateMarkdown(response))
+    })
 }
 
 // function call to initialize program
