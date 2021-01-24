34113637bd7a1a4cf1c515e455ff8d8d4681b530
Phlipper <marzullop73@gmail.com>
2021-01-24 17:27:23 -0500

    added list of questions to elicit user information

--- Develop/index.js
+++ Develop/index.js
@@ -7,17 +7,88 @@ const questions = [
     
             {
                 type: 'input',
-                message: 'what is your name',
+                message: 'Please enter your Project Title',
                 name: 'title',
+                projectTitle: 'Project Title',
                 
             },
             
             {
                 type: 'input',
-                message: 'what is your last name',
-                name: 'title',
+                message: 'Please enter your Project Description',
+                name: 'description',
+                description: 'Project Description',
+            },
+            
+            {
+                type: 'input',
+                message: 'Please enter Installation Instructions',
+                name: 'installation',
+                installationInstructions: 'Installation Instructions',
+            },
+
+            {
+                type: 'input',
+                message: 'Please enter Usage Information',
+                name: 'usage',
+                usageInformation: 'Usage Information',
+            
+            },
+        
+            {
+                type: 'input',
+                message: 'Please enter Contribution Guidelines',
+                name: 'contribution',
+                contributionGuidelines: 'Contribution Guidelines',
+            },
+        
+            {
+                type: 'input',
+                message: 'Please enter Test Instructions',
+                name: 'instructions',
+                testInstructions: 'Test Instructions',
+            },
+
+            {
+                type: 'list',
+                message: 'Please choose the applicable License(s)',
+                name: 'license',
+                choices: ["MIT", "APACHE2.0", "ISC", "NONE"],
+    
+            },
+
+            {
+                type: 'input',
+                message: 'Please enter your GitHub Username',
+                name: 'username',
                 
-            }
+            },
+
+            {
+                type: 'input',
+                message: 'Please enter your Email Address',
+                name: 'email',
+                
+            },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ];
