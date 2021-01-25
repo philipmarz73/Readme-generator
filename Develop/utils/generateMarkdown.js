591be2a6230a9c01e332bc85a7840d3d93b03cef
Phlipper <marzullop73@gmail.com>
2021-01-25 11:05:35 -0500

    added Table of Contents to generateMarkdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,49 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${createIcon(data.license)}
+
+  ## Table of Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Instructions](#instructions)
+  * [License](#license)
+  * [username](#username)
+  * [Email](#email)
+  * [Test](#test)
+  * [Questions](#questions)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 `;
 }
 
